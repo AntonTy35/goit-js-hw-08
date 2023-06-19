69ab5e4211457a8a1c0d7d6853023e545816f5f8
AntonTy35 <antony.tislenko@gmail.com>
2023-06-19 21:25:50 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(time => {
+    localStorage.setItem('videoplayer-current-time', time.seconds);
+  }, 1000)
+);
+
+const curentTime = localStorage.getItem('videoplayer-current-time');
+if (curentTime) {
+  player.setCurrentTime(curentTime);
+}
